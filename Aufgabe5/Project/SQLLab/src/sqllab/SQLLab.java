7167cc7b384d4565f1a7fff346aef5a205188311
nubix <me@guest124.ibr.cs.tu-bs.de>
2013-01-23 10:28:12 +0100

    .

---
+++ Aufgabe5/Project/SQLLab/src/sqllab/SQLLab.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sqllab;
+
+/**
+ *
+ * @author Jan Germann <j.germann@tu-bs.de>
+ */
+public class SQLLab {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+}
