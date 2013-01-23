7285d73f1446939cbe30eb28e0039380b310b8cb
nubix <me@guest124.ibr.cs.tu-bs.de>
2013-01-23 12:30:09 +0100

    AufgabeB

--- Aufgabe5/Project/SQLLab/src/sqllab/AufgabeB.java
+++ Aufgabe5/Project/SQLLab/src/sqllab/AufgabeB.java
@@ -15,22 +15,22 @@ import java.util.logging.Logger;
  * @author Jan Germann <j.germann@tu-bs.de>
  */
 public class AufgabeB {
-
+	private static String sqlQuery = "WITH himself(name, num) AS ( VALUES(?, 0) ), first_grade(name, num, actor1, title1) AS ( SELECT DISTINCT actor_to, 1, actor_from, movie FROM actor_cooccurrence WHERE actor_from IN (SELECT name FROM himself) UNION ALL (SELECT h.*, '', '' FROM himself h) ), second_grade( name, num, actor1, title1, actor2, title2 ) AS ( SELECT DISTINCT actor_to, 2, actor1, title1, actor_from, movie FROM first_grade AS f JOIN actor_cooccurrence ON actor_from = f.name UNION ALL (SELECT f.*, '', '' FROM first_grade AS f) ), third_grade( name, num, actor1, title1, actor2, title2, actor3, title3 ) AS ( SELECT DISTINCT actor_to, 3, actor1, title1, actor2, title2, actor_from, movie FROM actor_cooccurrence JOIN second_grade AS s ON actor_from = s.name UNION ALL (SELECT s.*,'','' FROM second_grade AS s) ) SELECT DISTINCT name, num, actor1, title1, actor2, title2, actor3, title3 FROM third_grade WHERE name = ? ORDER BY num ASC";
 	private static PreparedStatement getActorToActor;
 
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
-		doMenu();
-        
+	public static void main(String[] args) {        
         Connection conn;
+		
 		try {
 			Properties props = new Properties();
 			props.load(new FileInputStream(new File("connection.properties")));
 			conn = getConnection(props);
             
-            getActorToActor = conn.prepareStatement("WITH himself(name, num) AS ( VALUES(?, 0) ), first_grade(name, num, actor1, title1) AS ( SELECT DISTINCT actor_to, 1, actor_from, movie FROM actor_cooccurrence WHERE actor_from IN (SELECT name FROM himself) UNION ALL (SELECT h.*, '', '' FROM himself h) ), second_grade( name, num, actor1, title1, actor2, title2 ) AS ( SELECT DISTINCT actor_to, 2, actor1, title1, actor_from, movie FROM first_grade AS f JOIN actor_cooccurrence ON actor_from = f.name UNION ALL (SELECT f.*, '', '' FROM first_grade AS f) ), third_grade( name, num, actor1, title1, actor2, title2, actor3, title3 ) AS ( SELECT DISTINCT actor_to, 3, actor1, title1, actor2, title2, actor_from, movie FROM actor_cooccurrence JOIN second_grade AS s ON actor_from = s.name UNION ALL (SELECT s.*,'','' FROM second_grade AS s) ) SELECT DISTINCT name, num, actor1, title1, actor2, title2, actor3, title3 FROM third_grade WHERE name = ? ORDER BY num ASC");
+            getActorToActor = conn.prepareStatement(sqlQuery, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			System.out.println(getActorsConnection(getStdin("Actor 1: "), getStdin("Actor 2: "), conn));
 			
 		} catch(Exception ex) {
 			Logger.getLogger(AufgabeB.class.getName()).log(Level.SEVERE, null, ex);
@@ -41,11 +41,11 @@ public class AufgabeB {
 	 *
 	 * @return
 	 */
-	public static String doMenu() {
+	public static String getStdin(String caption) {
 		String s = "";
 		
 		try {
-			System.out.print("Please choose an actor:");
+			System.out.print(caption);
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			s = in.readLine();
@@ -55,15 +55,76 @@ public class AufgabeB {
 		} finally {
 			return s;
 		}
-
 	}
 
-	public static String getActorsConnection(String name1, String name2, Connection conn) throws SQLException {
+	/**
+	 * 
+	 * @param name1
+	 * @param name2
+	 * @param conn
+	 * @param skip
+	 * @return
+	 */
+	public static String getActorsConnection(String name1, String name2, Connection conn, int skip) throws SQLException {
+		int shortestPath = 0;
+		int connectionCount = 0;
+		String returnVal = "";
+		
 		getActorToActor.setString(1, name1);
 		getActorToActor.setString(2, name2);
-		ResultSet executeQuery = getActorToActor.executeQuery();
+		ResultSet result = getActorToActor.executeQuery();
+
+
+		if(result.next() == false)
+			return "I've not found anything you want my lord.";
+		shortestPath = result.getInt("NUM");
+
+
+		/*
+		 * Get the number of shortest paths
+		 * and ask which one to show if there's more than one
+		 */
+		while(shortestPath == result.getInt("NUM")) {
+			connectionCount++;
+			result.next();
+		}
+		result.first();
+//		result.next();
 		
-		return "";
+		if(connectionCount > 1) {
+			System.out.println("There are " + connectionCount + " possible paths.");
+			String s = getStdin("Which one to show? Number: ");
+			skip = Integer.parseInt(s) - 1 ;
+		}
+
+
+		/*
+		 * Possibility to show other paths
+		 */
+		for(;skip>0; skip--) {
+			result.next();
+			if(result.getInt("NUM") > shortestPath) {
+				result.first();
+//				result.next();
+				break;
+			}
+		}
+
+		returnVal += "Shortest path has " + result.getString("NUM") + " hops\n";
+		for(int i=1; result.getInt("NUM") >= i; i++)
+			returnVal += result.getString("ACTOR"+i) + " -> "
+					  + result.getString("TITLE"+i) +  " -> " ;
+
+		returnVal += result.getString("NAME");
+
+		return returnVal;
+	}
+
+	/**
+	 * Same as getActorsConnection(String, String, Connection, int)
+	 */
+	public static String getActorsConnection(String name1, String name2, Connection conn) throws SQLException {
+		return getActorsConnection(name1, name2, conn, 0);
 	}
 
 	/**
